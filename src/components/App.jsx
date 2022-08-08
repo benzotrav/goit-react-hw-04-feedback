9ad964767bb79d2d6e1f673eeff56361a906362b
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-08-08 18:40:09 +0100

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,61 @@
-import React, { Component } from 'react';
 import { Feedback } from "./Section/Section";
 import { Statistics } from './Statistics/Statistics';
 import { Options } from './Options/Options';
+import { useState } from 'react';
 
 
 
-export class App extends Component {
-    state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };    
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
+  const choseAnswer = answearValue => {
+    switch (answearValue) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
 
-    choseAnswer = answearValue => {
-        this.setState(prevState => {
-          return { [answearValue]: prevState[answearValue] + 1 };
-        });
-      };
-    
-      countTotalFeedback() {
-        const answearValues = Object.values(this.state);
-    
-        return answearValues.reduce((acc, item) => {
-          return acc + item;
-        }, 0);
-      }
-    
-      countPositiveFeedbackPercentage() {
-        const totalFeedback = this.countTotalFeedback();
-        const positiveFeedback = this.state.good;
-    
-        if (positiveFeedback === 0) {
-          return Number(0);
-        } else {
-          return Number(((positiveFeedback / totalFeedback) * 100).toFixed(0));
-        }
-      }
+      case 'bad':
+        setBad(state => state + 1);
+        break;
 
-  
-    render() {  
-        const buttonsArray = Object.keys(this.state);
-        const statsArray = Object.entries(this.state);
-        const totalAnswears = this.countTotalFeedback();
-        const avarageValue = this.countPositiveFeedbackPercentage();
+      default:
+        break;
+    }
+  };
+
+  const totalAnswears = good + neutral + bad;
+  const avarageValue =
+    good === 0 ? Number(0) : Number(((good / totalAnswears) * 100).toFixed(0));
 
       return (
         <div>
           <Feedback title="Please leave feedback">
           </Feedback>
-          <Options
-            options={buttonsArray}
-            onLeaveFeedback={this.choseAnswer}
+          <Options 
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={choseAnswer}
           />
           <Feedback title="Statistics">    
           {totalAnswears === 0 ? (
             <h1>No feedback given</h1>
           ) : (
             <Statistics
-              total={totalAnswears}
-              positivePercentage={avarageValue}
-              statsArray={statsArray}
+            total={totalAnswears}
+            positivePercentage={avarageValue}
+            statsArray={[
+              ['good', good],
+              ['neutral', neutral],
+              ['bad', bad],
+            ]}
             />
           )}        
           </Feedback>
         </div>
       );
     }
-  }
+  
